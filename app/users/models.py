f2ee8a541dfb46f408930c0a468045dd4a6636c5
oli <olivrg@gmail.com>
2019-05-04 15:51:23 +0100

    add custom user model

--- app/users/models.py
+++ app/users/models.py
@@ -52,3 +52,55 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """custom user model"""
+    random_user_id = models.CharField(
+        'Random User Id',
+        editable=False,
+        max_length=32,
+        default=helper.random_id,
+        unique=True,
+        primary_key=True
+    )
+    username = models.CharField(
+        db_index=True,
+        verbose_name='Username',
+        unique=True,
+        max_length=255,
+        help_text=_(
+            'Required. 255 characters or fewer. Letters, numbers and \
+            @/./+/-/_ characters'),
+        validators=[
+            validators.RegexValidator(
+                re.compile('^[\w.@+-]+$'),
+                _('Enter a valid username.'),
+                _('invalid')
+            )
+        ]
+    )
+    email = models.EmailField(
+        db_index=True, verbose_name='Email', max_length=255, unique=True)
+    name = models.CharField(verbose_name='Name', max_length=60)
+    favourite_dish = models.CharField(
+        'Favourite dish', max_length=60, blank=True, null=True)
+    date_joined = models.DateTimeField(
+        verbose_name='Updated At', auto_now=True)
+    is_active = models.BooleanField(verbose_name='Active User', default=True)
+    is_staff = models.BooleanField(verbose_name='Staff User', default=False)
+    profile_picture = models.CharField(max_length=120, blank=True, default="")
+    following = models.ManyToManyField(
+        "self", related_name='following_field', blank=True, symmetrical=False)
+    followers = models.ManyToManyField(
+        "self", related_name='followers_field', blank=True, symmetrical=False)
+    reset_password_token = models.CharField(
+        max_length=120, blank=True, null=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'name']
+
+    objects = UserManager()
+
+    def __unicode__(self):
+        return smart_text(self.name)
