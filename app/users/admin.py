d4f2e338a124b18770e2d965aeab03e606e58371
oli <olivrg@gmail.com>
2019-05-11 20:21:24 +0100

    add user creation form to admin

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,3 +1,35 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.models import Group
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from users.models import User
+
+
+class UserCreationForm(forms.ModelForm):
+    """form to create new users"""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User 
+        fields = ("email",)
+    
+    def clean_password2(self):
+        # check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
