f458706e68cf0027acee83aebd03d939d12af635
oli <olivrg@gmail.com>
2019-05-17 11:19:52 +0100

    add post, comment and report serializers

--- app/posts/serializers.py
+++ app/posts/serializers.py
@@ -1,5 +1,6 @@
-from posts.models import Comment, Post, Report
 from rest_framework import serializers
+
+from posts.models import Comment, Post, Report
 from users.models import User
 
 
@@ -23,4 +24,56 @@ class PostLikesSerializer(serializers.ModelSerializer):
         fields = [
             'random_user_id',
             'username',
+        ]
+
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    url = PostUrlField(view_name='posts:PostDetailAPIView')
+    user = PostUserSerializer()
+    likes = PostLikesSerializer(many=True)
+
+    class Meta:
+        model = Post
+        fields = [
+            'url',
+            'random_post_id',
+            'owner',
+            'image',
+            'caption',
+            'timestamp',
+            'likes',
+        ]
+
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = [
+            'owner',
+            'image',
+            'caption',
+        ]
+
+
+class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Comment
+        fields = [
+            'random_comment_id',
+            'post',
+            'owner',
+            'content',
+            'timestamp',
+        ]
+
+
+class ReportSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Report
+        fields = [
+            'random_report_id',
+            'post',
+            'user',
+            'message',
+            'timestamp',
         ]
