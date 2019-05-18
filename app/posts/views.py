2743e724c9e6f556bb391065ddbb12288411c791
oli <olivrg@gmail.com>
2019-05-18 20:04:02 +0100

    add create like api

--- app/posts/views.py
+++ app/posts/views.py
@@ -14,3 +14,19 @@ from posts.serializers import (CommentSerializer, PostCreateSerializer,
                                PostLikesSerializer, PostSerializer,
                                ReportSerializer)
 from users.models import User
+
+
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication, ])
+@permission_classes([])
+def like_create_api(request, *args, **kwargs):
+    serializer = PostLikesSerializer()
+    post = get_object_or_404(
+        Post, random_post_id=request.data['random_post_id'])
+    post.likes.add(request.data['random_user_id'])
+
+    liker = get_object_or_404(
+        User, random_user_id=request.data['random_user_id'])
+    likee = get_object_or_404(User, random_user_id=post.user.random_user_id)
+
+    return Response(serializer.data, status=HTTP_200_OK)
